d8c2283efd227e8e2d46e981fc28e1f63cb2ffb3
Koen Pauwels <koenpauwels2@gmail.com>
2021-06-28 21:08:11 +0200

    abis, feats, max hp calculation

--- yappa.pl
+++ yappa.pl
@@ -1,13 +1,33 @@
 :- discontiguous
+       gain_level/3,
        spell/2,
        spell/3,
        describe/2,
        feature/2,
        feature_options/4,
-       problem/2.
+       class_option/1,
+       problem/2,
+       todo/1.
+:- op(500, xfx, d).
+
+die_avg(X d Y, Avg) :- Avg is ceiling(X * (Y+1) / 2).
 
 sum([], 0).
 sum([X|Xs], Sum) :- sum(Xs, SumXs), Sum is X + SumXs.
+
+class_option(fighter).
+class_option(paladin).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Feats.
+feature(feat(alert), add_initiative(5)).
+
+% TODO denken over: feature(F), describe(F,D)
+describe(feat(alert), "Always on the lookout for danger, you gain the following benefits:
+    - You can’t be surprised while you are conscious.
+    - You gain a +5 bonus to initiative.
+    - Other creatures don’t gain advantage on attack rolls against you as a result of being hidden from you.
+").
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Spells.
@@ -68,11 +88,25 @@ describe(shell_defense, "You can withdraw into your shell as an action. Until yo
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Druid class features.
-hd_per_level(druid, d(1,8)).
+class_option(druid).
+hd_per_level(druid, 1 d 8).
+max_hp_initial(druid, 8).
+max_hp_per_level(druid, 1 d 8).
+
 % hp TODO
 % initial_hp(druid, )
 saving_throw(druid, int).
 saving_throw(druid, wis).
+
+gain_spell_slots(druid, spell_level(1), [1,1,2,3]).
+gain_spell_slots(druid, spell_level(2), [3,3,4]).
+gain_spell_slots(druid, spell_level(3), [5,5,6]).
+gain_spell_slots(druid, spell_level(4), [7,8,9]).
+gain_spell_slots(druid, spell_level(5), [7,8,9]).
+gain_spell_slots(druid, spell_level(6), [11,19]).
+gain_spell_slots(druid, spell_level(7), [13,20]).
+gain_spell_slots(druid, spell_level(8), [15]).
+gain_spell_slots(druid, spell_level(9), [17]).
 
 druid_cantrips(Cantrips) :- 
     findall(Cantrip, (spell(Cantrip, level, 0), spell(Cantrip, class, druid)), Cantrips).
@@ -124,15 +158,13 @@ describe(wild_shape, "You can’t cast spells, and your ability to speak or take
 describe(wild_shape, "You retain the benefit of any features from your class, race, or other source and can use them if the new form is physically capable of doing so. However, you can’t use any of your special senses, such as darkvision, unless your new form also has that sense.").
 describe(wild_shape, "You choose whether your equipment falls to the ground in your space, merges into your new form, or is worn by it. Worn equipment functions as normal, but the GM decides whether it is practical for the new form to wear a piece of equipment, based on the creature’s shape and size. Your equipment doesn’t change size or shape to match the new form, and any equipment that the new form can’t wear must either fall to the ground or merge with it. Equipment that merges with the form has no effect until you leave the form.").
 
+%feature_options(class(druid,4), ability_score_improvement, 2, Options) :-
+%    findall(add_ability(Ability,1), ability(Ability), Options).
 feature_options(class(druid,4), druid_cantrips, 1, Cantrips) :-
     druid_cantrips(Cantrips).
 
     
 % Logic for picking options. (TODO move)
-problem(pick(Condition, Name), no_option_picked) :-
-    matched(Condition),
-    feature_options(Condition, Name, _, _),
-    \+ pick(Condition, Name, _).
 problem(pick(Condition, Name), no_such_option) :-
     pick(Condition, Name, _),
     \+ feature_options(Condition, Name, _, _).
@@ -150,9 +182,22 @@ problem(pick(Condition, Name), invalid_option(Pick)) :-
     member(Pick, Selection),
     \+ member(Pick, Options).
 
+todo(no_option_picked(Condition, Name)) :-
+    feature_options(Condition, Name, _, _),
+    matched(Condition),
+    \+ pick(Condition, Name, _).
+todo(unsolved_problems(Problem, Error)) :-
+    problem(Problem, Error).
+
 valid_pick(Condition, Name, Selection) :-
     pick(Condition, Name, Selection),
     \+ problem(pick(Condition, Name), _).
+
+% A bit of a hack, perhaps.
+valid_pick_at_level(CharLevel, Condition, Name, Selection) :-
+    matched_at_level(CharLevel, Condition),
+    pick(Condition, Name, Selection),
+    problem(pick(Condition, Name), Reason) -> Reason = condition_unmatched.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Items.
@@ -164,47 +209,149 @@ body_armor(halfplate, medium, ac(15), [disadvantage(stealth)]).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Features.
-matched(class(Class)) :- class(Class).
-matched(class(Class,ClassLevel1)) :- class(Class, ClassLevel2), ClassLevel1 =< ClassLevel2.
-matched(race(Race)) :- race(Race).
+matched(Condition) :- level(CharLevel), matched_at_level(CharLevel, Condition).
+matched_at_level(CharLevel, class(Class)) :-
+    class_at_level(CharLevel, Class, _).
+matched_at_level(CharLevel, class(Class,ClassLevel1)) :-
+    class_at_level(CharLevel, Class, ClassLevel2),
+    between(1, ClassLevel2, ClassLevel1).
+matched_at_level(_, race(Race)) :-
+    race(Race).
+matched_at_level(CharLevel, feat(Feat)) :-
+    pick_feat(Level, Feat),
+    between(1, CharLevel, Level).
 
-feature(Feature) :-
+feature(Feature) :- level(CharLevel), feature_at_level(CharLevel, Feature).
+feature_at_level(CharLevel, Feature) :-
     feature(Condition, Feature),
-    matched(Condition),
-    !.
-feature(Feature) :- 
-    valid_pick(_, _, Feature),
-    !.
+    matched_at_level(CharLevel, Condition).
+feature_at_level(CharLevel, Feature) :-
+    valid_pick_at_level(CharLevel, _, _, Feature).
+feature_at_level(CharLevel, add_ability(Abil,Val)) :-
+    valid_increase_ability_score_at_level(CharLevel, _, Abil, Val).
 
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Options by leveling.
+ability_score_increase_level(Level) :- member(Level, [4,8,12,16,19]).
+
+valid_increase_ability_score(PickLevel, Abil, Val) :-
+    level(CharLevel),
+    valid_increase_ability_score_at_level(CharLevel, PickLevel, Abil, Val).
+valid_increase_ability_score_at_level(CharLevel, PickLevel, Abil, Val) :-
+    increase_ability_score(PickLevel, Abil, Val),
+    PickLevel =< CharLevel,
+    \+ problem(increase_ability_score(PickLevel, Abil, Val), _).
+
+% Detect unspent ability score increases.
+todo(ability_score_increase_unspent(level(Level))) :-
+    ability_score_increase_level(Level),
+    level(CharLevel),
+    Level =< CharLevel,
+    \+ (increase_ability_score(Level, _, _) ; pick_feat(Level, _)).
+
+% Detect if we tried to add an abi on an inappropriate level.
+problem(increase_ability_score(Level, Abil, Val), no_abi_level(Level)) :-
+    increase_ability_score(Level, Abil, Val),
+    \+ ability_score_increase_level(Level).
+
+% Detect if we tried to add a feat on an inappropriate level.
+problem(pick_feat(Level, Feat), no_abi_level(Level)) :-
+    pick_feat(Level, Feat),
+    \+ ability_score_increase_level(Level).
+
+% Detect if ability score increases don't sum up to 2.
+problem(increase_ability_score(Level, Abil, Val), not_equal_to_2) :-
+    increase_ability_score(Level, Abil, Val),
+    findall(Val, increase_ability_score(Level, _, Val), Vals),
+    sum(Vals, SumVals),
+    SumVals \= 2.
+
+% Detect if ability score increases have been picked that the PC is
+% not eligible for.
+problem(increase_ability_score(Level, Abil, Val), level_too_low) :-
+    increase_ability_score(Level, Abil, Val),
+    level(CharLevel),
+    CharLevel < Level.
+
+% Detect if the player selected both a feat and an ability score increase.
+problem(increase_ability_score(Level, Abil, Val), picked_both_feat_and_abi) :-
+    problem(picked_both_feat_and_abi(Level,Abil,Val,_), picked_both_feat_and_abi).
+problem(pick_feat(Level,Feat), picked_both_feat_and_abi) :-
+    problem(picked_both_feat_and_abi(Level,_,_,Feat), picked_both_feat_and_abi).
+problem(picked_both_feat_and_abi(Level,Abil,Val,Feat), picked_both_feat_and_abi) :-
+    increase_ability_score(Level, Abil, Val),
+    pick_feat(Level, Feat).
+
+
+% TODO: Detect if ability score increase pushes the ability over 20.
+% Should figure out what kind of things can affect ability scores though.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Character sheet calculations.
 
+% Hit points.
+max_hp(HP) :-
+    findall(Term, hp_term_for_level(_, Term), Terms),
+    sumlist(Terms, HP).
+
+hp_term_for_level(1, Term) :-
+    initial_class(Class),
+    max_hp_initial(Class, Init),
+    ability_at_level(1, con, Con),
+    mf(Con, ConMod),
+    Term is Init + ConMod.
+hp_term_for_level(CharLevel, Term) :-
+    gain_level(CharLevel, Class, HPMode),
+    max_hp_per_level(Class, HPDie),
+    hp_die(HPMode, HPDie, ClassHP),
+    ability_at_level(1, con, Con),
+    mf(Con, ConMod),
+    Term is ClassHP + ConMod.
+
+hp_die(hp_avg, Die, HP) :-
+    die_avg(Die, HP).
+hp_die(hp_rolled(HP), _, HP).
+
 % Abilities and modifiers.
+ability(str).
+ability(dex).
+ability(con).
+ability(wis).
+ability(int).
+ability(cha).
 ability(Abil,Val) :-
-    base_ability(Abil,Base),
-    findall(Term, feature(add_ability(Abil,Term)), Terms),
-    sum(Terms, SumTerms),
-    Val is Base + SumTerms.
+    level(CharLevel),
+    ability_at_level(CharLevel, Abil, Val).
+ability_at_level(CharLevel, Abil, Val) :-
+    base_ability(Abil, Base),
+    findall(Term, feature_at_level(CharLevel, add_ability(Abil,Term)), Terms),
+    sumlist([Base | Terms], Val).
 
 mf(Val, Mod) :-
     floor( (Val-10) / 2, Mod ).
 
-ability_modifier(Abil,Mod) :-
-    ability(Abil,Val),
-    mf(Val,Mod).
+ability_mod(Abil, Mod) :-
+    ability(Abil, Val),
+    mf(Val, Mod).
 
 % Armor class.
 ac(AC) :- feature(natural_armor(AC)), !.
 ac(AC) :- equipped(Armor), body_armor(Armor, heavy, ac(AC), _), !.
 ac(AC) :- equipped(Armor), body_armor(Armor, medium, ac(ArmorAC), _), !,
-          ability_modifier(dex, Mod),
+          ability_mod(dex, Mod),
           AC is ArmorAC + min(Mod,2).
 ac(AC) :- equipped(Armor), body_armor(Armor, light, ac(ArmorAC), _), !,
-          ability_modifier(dex, Mod),
+          ability_mod(dex, Mod),
           AC is ArmorAC + Mod.
-ac(AC) :- ability_modifier(dex, Mod),
+ac(AC) :- ability_mod(dex, Mod),
           AC is 10 + Mod.
+
+% Initiative.
+initiative_mod(Init) :-
+    ability_mod(dex, Mod),
+    findall(Term, feature(add_initiative(Term)), Terms),
+    sumlist([Mod | Terms], Init).
 
 % Proficiency bonus.
 calc_bonus(Level, Bonus) :- Bonus is 2 + div(Level-1, 4).
@@ -213,33 +360,47 @@ proficiency_bonus(Bonus) :- level(Level), calc_bonus(Level, Bonus).
 % Spellcasting.
 spell_save_dc(DC) :-
     feature(spellcasting(Abil)),
-    ability_modifier(Abil, Mod),
+    ability_mod(Abil, Mod),
     proficiency_bonus(Bonus),
     DC is 8 + Bonus + Mod.
 
 spell_attack_modifier(Mod) :-
     feature(spellcasting(Abil)),
-    ability_modifier(Abil, AbilMod),
+    ability_mod(Abil, AbilMod),
     proficiency_bonus(Bonus),
     Mod is Bonus + AbilMod.
 
+spell_slots(Class, SpellLevel, Slots) :-
+    gain_spell_slots(Class, SpellLevel, Gains),
+    class(Class, ClassLevel),
+    findall(X, (member(X,Gains),X=<ClassLevel), Xs),
+    length(Xs, Slots),
+    Slots > 0.
+
 % Levels.
 level(Level) :-
-    findall(L, gain_level(L,_), Levels),
+    findall(L, gain_level(L,_,_), Levels),
     max_member(Level, Levels).
 
 class(Class) :-
     initial_class(Class).
 class(Class) :-
-    findall(C, gain_level(_,C), Classes),
+    findall(C, gain_level(_,C,_), Classes),
     list_to_set(Classes, ClassesSet),
     member(Class, ClassesSet).
-
-class(Class, Level) :-
-    findall(L, gain_level(L,Class), Levels),
+class(Class, ClassLevel) :-
+    level(CharLevel),
+    class_at_level(CharLevel, Class, ClassLevel).
+class_at_level(CharLevel, Class, ClassLevel) :-
+    class_option(Class),
+    findall(L, (between(2,CharLevel,L),gain_level(L,Class,_)), Levels),
     (initial_class(Class) -> X = 1 ; X = 0),
     length(Levels, Len),
-    Level is Len + X.
+    ClassLevel is Len + X,
+    ClassLevel > 0.
+
+feat(Feat) :-
+    matched(feat(Feat)).
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -273,7 +434,7 @@ skill_proficiency_bonus(Skill, 0) :- \+ proficient(Skill).
 
 skill(Skill, Score) :-
     skill_ability(Skill, Abil),
-    ability_modifier(Abil, Mod),
+    ability_mod(Abil, Mod),
     skill_proficiency_bonus(Skill, Bonus),
     Score is Mod + Bonus.
 
@@ -281,14 +442,25 @@ skill(Skill, Score) :-
 % Character facts.
 
 initial_class(druid).
-gain_level(2, druid).
-gain_level(3, druid).
-gain_level(4, druid).
-gain_level(5, druid).
-gain_level(6, druid).
+
+gain_level(2, druid, hp_avg).
+
+gain_level(3, druid, hp_avg).
+
+gain_level(4, druid, hp_avg).
+pick_feat(4, alert).
+pick_feat(_,_) :- false.
+increase_ability_score(_,_,_) :- false.
+%increase_ability_score(3,dex,2).
+%increase_ability_score(4,dex,2).
+
+gain_level(5, druid, hp_avg).
+
+gain_level(6, druid, hp_avg).
+
 
 problem(gain_level, not_contiguous(Levels)) :-
-    findall(L, gain_level(L,_), Levels),
+    findall(L, gain_level(L,_,_), Levels),
     max_member(Highest, Levels),
     findall(L, between(2,Highest,L), Levels2),
     Levels \= Levels2.
@@ -313,6 +485,10 @@ equipped(_) :- false.
 problems :-
     findall((P,E), problem(P,E), Problems),
     maplist(writeln, Problems).
+
+todo :-
+    findall(T, todo(T), Todos),
+    maplist(writeln, Todos).
 
 ac :- ac(AC), writeln(AC).
 
