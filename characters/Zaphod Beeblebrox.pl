73cf88716757749bcdb28f0c0d73c3ebd6907ab6
PieterV2406 <pieter.verlinden@ae.be>
2022-04-24 22:20:32 +0200

    Initial Vue setup

--- characters/Zaphod Beeblebrox.pl
+++ characters/Zaphod Beeblebrox.pl
@@ -1,7 +1,8 @@
 name('Zaphod Beeblebrox').
+base_ability(str,10).
+base_ability(dex,10).
 base_ability(con,10).
 base_ability(wis,10).
 base_ability(int,10).
 base_ability(cha,10).
-base_ability(str,15).
-base_ability(dex,0).
+choice(init,'base race',elf).
