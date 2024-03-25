79b7936c2c58c1085260b040a7deaf48d70a19f8
Koen Pauwels <koenpauwels2@gmail.com>
2024-03-25 20:15:11 +0100

    wl.pl fixes

--- characters/wl.pl
+++ characters/wl.pl
@@ -135,15 +135,15 @@ spell_auto_data(hex, properties{ level: 1,
                                  range: feet(90),
                                  casting_time: "1 bonus action",
                                  duration: "1 hour",
-                                 concentration: true,
-                                 ritual: false,
-                                 desc: "You place a curse on a creature that you can see within range. Until the spell ends, you deal an extra 1d6 necrotic damage to the target whenever you hit it with an attack. Also, choose one ability when you cast the spell. The target has disadvantage on ability checks made with the chosen ability.
+                                 concentration: yes,
+                                 ritual: no,
+                                 desc: ["You place a curse on a creature that you can see within range. Until the spell ends, you deal an extra 1d6 necrotic damage to the target whenever you hit it with an attack. Also, choose one ability when you cast the spell. The target has disadvantage on ability checks made with the chosen ability.
 
 If the target drops to 0 hit points before this spell ends, you can use a bonus action on a subsequent turn of yours to curse a new creature.
 
 A Remove Curse cast on the target ends this spell early.
 
-At Higher Levels. When you cast this spell using a spell slot of 3rd or 4th level, you can maintain your concentration on the spell for up to 8 hours. When you use a spell slot of 5th level or higher, you can maintain your concentration on the spell for up to 24 hours.",
+At Higher Levels. When you cast this spell using a spell slot of 3rd or 4th level, you can maintain your concentration on the spell for up to 8 hours. When you use a spell slot of 5th level or higher, you can maintain your concentration on the spell for up to 24 hours."],
                                  classes: [warlock],
                                  damage_with_cantrip_scaling: false,
                                  damage_at_slot_level: false,
